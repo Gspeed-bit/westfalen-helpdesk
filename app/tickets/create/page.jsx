e5b64c74e31825362f69e0d4aa9960a3b50d4238
Ayodeji Daniel Adeyeye <adeyeye17@gmail.com>
2024-03-26 00:04:21 +0100

    adding ticket page done

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,11 @@
+import CreateForm from "./CreateForm";
+
+const createTicket = () => {
+  return (
+    <main >
+      <h2 className="text-center">Add a new Ticket</h2>
+      <CreateForm />
+    </main>
+  );
+};
+export default createTicket;
